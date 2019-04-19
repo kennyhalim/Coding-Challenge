302680c8acce32daf75545f6a9a258f541bcc6c9
Kenny Halim <kennyhalim98@gmail.com>
2019-04-19 11:27:19 -0700

    finish code

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,6 @@ $(document).ready(function() {
     request.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
         const response = JSON.parse(this.responseText);
-        console.log(response);
         getElements(response);
       }
     }
@@ -17,6 +16,8 @@ $(document).ready(function() {
 
    const getElements = function(response) {
       $("#output").empty();
+      $(".intro").hide();
+      $(".result").show();
       response.body.data.forEach(function(dat){
         $("#output").append('<div class="col-md-6"><h2>' + dat.name + '</h2> <img src="' + dat.imageUrl + '"/> <p>' + dat.description +'</p> <p> Stock: ' + dat.stock + '</p> <p>Price: <strong>$' + dat.cost + '</strong></p></div>');
       });
@@ -47,5 +48,37 @@ $(document).ready(function() {
         }    
       });
     }
+  });
+
+  $('#filter').change(function() {
+    let request = new XMLHttpRequest();
+    let filterValue = $("#filter").val();
+    console.log(filterValue);
+    const url = `https://it771mq5n2.execute-api.us-west-2.amazonaws.com/production/furniture`;
+    
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    }
+
+    request.open("GET", url, true);
+    request.send();
+
+   const getElements = function(response) {
+      $("#output").empty();
+      response.body.data.forEach(function(dat){
+        if(filterValue === "none"){
+          $("#output").append('<div class="col-md-6"><h2>' + dat.name + '</h2> <img src="' + dat.imageUrl + '"/> <p>' + dat.description +'</p> <p> Stock: ' + dat.stock + '</p> <p>Price: <strong>$' + dat.cost + '</strong></p></div>');
+        } else if(parseInt(dat.cost) <= parseInt(filterValue)){
+          $("#output").append('<div class="col-md-6"><h2>' + dat.name + '</h2> <img src="' + dat.imageUrl + '"/> <p>' + dat.description +'</p> <p> Stock: ' + dat.stock + '</p> <p>Price: <strong>$' + dat.cost + '</strong></p></div>');
+        }      
+      });
+    }
+  });
+
+  $("#header").click(function(){
+    location.reload();
   });
 });
